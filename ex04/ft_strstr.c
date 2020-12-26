a2fb31f32bf716a10f0d504e87263dc4bae0c65f
katrin19 <58865678+katrin19@users.noreply.github.com>
2020-12-26 23:59:03 +0400

    c03

---
+++ ex04/ft_strstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpetrosy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/06 15:38:46 by kpetrosy          #+#    #+#             */
+/*   Updated: 2020/12/06 15:38:49 by kpetrosy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdbool.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	char *haystack;
+	char *needle;
+
+	if (*to_find == '\0')
+		return (str);
+	haystack = str;
+	needle = to_find;
+	while (true)
+	{
+		if (*needle == '\0')
+			return ((char *)(haystack - (needle - to_find)));
+		if (*haystack == *needle)
+			needle++;
+		else
+			needle = to_find;
+		if (*haystack == '\0')
+			break ;
+		haystack++;
+	}
+	return (0);
+}
